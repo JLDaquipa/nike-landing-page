69698d7d2740b1e500ed716f7e3a4a968cc3f429
JLDaquipa <JLPDfive5@gmail.com>
2023-11-02 08:26:20 +0800

    Modified Special Offer, Subscribe, Customer Reviews and Footer sections

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,18 +1,27 @@
 import Button from "../components/Button"
+import {offer} from "../assets/images"
+import { arrowRight } from "../assets/icons"
 
 const SpecialOffer = () => {
   return (
     <section className="flex justify-center items-center max-xl:flex-col-reverse gap-10 max-container ">
-      <div>
-        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-            We Provide You
-            <span className="text-coral-red"> Super </span>
-            <span className="text-coral-red">Quality</span> Shoes
+      <div className="flex-1">
+        <img src={offer} width={773} height={687} className="object-containe w-full"/>
+      </div>
+      <div className="flex flex-1 flex-col">
+          <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+            <span className="text-coral-red"> Special </span> Offer
           </h2>
-          <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
-          <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
-          <div className="mt-11">
-            <Button label="View details" />
+          <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart</p>
+          <p className="mt-6 lg:max-w-lg info-text">Navigate a realm og possibilities designed to fulfill your unique desire, surparssing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
+          <div className="flex flex-wrap gap-4 mt-11">
+            <Button label="Shop now" iconURL={arrowRight}/>
+            <Button 
+              label="Learn more"
+              backgroundColor="bg-white"
+              borderColor="border-slate-gray"
+              textColor="text-slate-gray" 
+            />
           </div>
       </div>
     </section>
